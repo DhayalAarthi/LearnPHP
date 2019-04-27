222324355e609ba81546f1db6d67a4328a75c114
Dhayal Ram <dhayalram.k@gmail.com>
2019-04-27 15:25:17 +0530

    Adding Booleans

--- Laracasts/The_PHP_Practitioner/8_Booleans/index.view.php
+++ Laracasts/The_PHP_Practitioner/8_Booleans/index.view.php
@@ -11,5 +11,12 @@
 				echo '<li><strong>'.ucwords($heading).'</strong>: '.$value.'</li>';
 			} ?>
 		</ul>
+
+		<ul>
+			<li><strong>Title</strong> <?= $task['title'];?></li>
+			<li><strong>Due</strong> <?= $task['due'];?></li>
+			<li><strong>Assigned To</strong> <?= $task['assigned_to'];?></li>
+			<li><strong>Completed</strong> <?= $task['completed'] ? 'Complete' : 'InComplete';?></li>
+		</ul>
 	</body>
 </html>
