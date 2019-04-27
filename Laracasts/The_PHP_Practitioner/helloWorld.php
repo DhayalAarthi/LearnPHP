8cb786dc514f32d2a7807deac7a1b9b1cce18a49
Dhayal Ram <dhayalram.k@gmail.com>
2019-04-27 13:32:50 +0530

    Hello World Program
    Created Hello World Program in PHP and written with description

---
+++ Laracasts/The_PHP_Practitioner/helloWorld.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+*	Hello World simple program
+*/
+echo 'Hello World';
+
+/*
+*	Open Terminal or command prompt
+*	Go to the location
+*	
+*	$ php helloWorld.php
+*
+*	Output: Hello World
+*
+*	echo means printing - Browser or console/terminal
+*	
+*	It will work only when we give <?php tag, else it will just throw to output.
+*	When we give <?php it will start interpreting
+*
+*	'Hello World' - it's just a string of characters, we can do in single and double quotes. 
+*	There is minor difference between single quotes and double quotes
+*	It's not a function/procedure
+*
+*	
+*/
+
