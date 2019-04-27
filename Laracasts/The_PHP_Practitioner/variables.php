007c37b185c32b90db6bd3dbdef366cd3b540ce9
Dhayal Ram <dhayalram.k@gmail.com>
2019-04-27 13:52:45 +0530

    Inseting Variables lession

---
+++ Laracasts/The_PHP_Practitioner/variables.php
@@ -0,0 +1,22 @@
+<?php
+
+// Create variable by specifying with $ sign
+
+$greeting = 'Hello Universe';
+
+echo $greeting;
+
+$name = 'Dhayal Ram';
+echo "Hello $name";
+echo "Hello {$name}"; // To make developers visible
+
+// PHP using period "." to concat the string
+
+
+
+$greeting = 'Hello';
+$name = 'Dhayal Ram';
+echo $greeting.', '.$name;
+
+echo "$greeting, $name";
+echo "{$greeting}, {$name}";
